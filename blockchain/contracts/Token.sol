5a273ba05cceb77e5487bb29b77320fb0db0ab7f
amirshapkota <shapkotaamir0@gmail.com>
2022-08-05 21:43:03 +0545

    Created Approve Function A function to approve the transactions

--- blockchain/contracts/Token.sol
+++ blockchain/contracts/Token.sol
@@ -24,4 +24,11 @@ contract Token {
         return true;
     }
 
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+
+        allowance[msg.sender][_spender] = _value;
+
+        return true;
+    }
+
 }
