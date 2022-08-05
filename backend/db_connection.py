8a8ed654017fb3aa67699207c4d1810d25b4c659
Ashwot-Acharya <ashwot1133@gmail.com>
2022-08-05 21:32:06 +0545

    Merchant model added Mearchants Schema is used to register merchants/selle,  once the admin approves mearchant request

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -1,7 +1,8 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base , sessionmaker , relationship
 from sqlalchemy import create_engine
 from sqlalchemy import Column , String , DateTime, Integer, ForeignKey
 from datetime import datetime
+from sqlalchemy_utils import EmailType , PasswordType 
 import os 
 from sqlalchemy import ForeignKey
 Base = declarative_base()
@@ -18,7 +19,17 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer() , primary_key = True)
     name = Column(String(50), nullable = False, unique=True)
-    email = Column(String(150), unique = True , nullable =False)
-    adress = Column(String(150), nullable = False),
-    password = Column(String(100), nullable = False)
+    email = Column(EmailType(150), unique = True , nullable =False)
+    adrress = Column(String(150), nullable = False),
+    password = Column(PasswordType(schemes=['pbkdf2_sha512','md5_crypt'],deprecated=['md5_crypt']), nullable = False)
     date_create = Column(DateTime() , default = datetime.utcnow)
+
+class Mearhant(Base):
+    __tablename__ = "mearchants"
+    id = Column(Integer() , primary_key = True)
+    name = Column(String(50), nullable = False, unique=True)
+    email = Column(EmailType(150), unique = True , nullable =False)
+    adrress = Column(String(150), nullable = False),
+    password = Column(PasswordType(schemes=['pbkdf2_sha512','md5_crypt'],deprecated=['md5_crypt']), nullable = False)
+    date_create = Column(DateTime() , default = datetime.utcnow)
+    
