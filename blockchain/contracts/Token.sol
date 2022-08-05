1aded5a69774d8bc94c4b97c2f85166a80c39d79
amirshapkota <shapkotaamir0@gmail.com>
2022-08-05 21:47:26 +0545

    Created transferFrom function A function which handles user transfering token from one to other

--- blockchain/contracts/Token.sol
+++ blockchain/contracts/Token.sol
@@ -31,4 +31,16 @@ contract Token {
         return true;
     }
 
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        
+        return true;
+    }
+
 }
