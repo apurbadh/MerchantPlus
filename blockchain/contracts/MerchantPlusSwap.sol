91df8dffe19f271f318a3f58e20895bc7be32a18
amirshapkota <shapkotaamir0@gmail.com>
2022-08-05 22:08:21 +0545

    Merchant Plus Swap Smart Contract for exchanging with the token

---
+++ blockchain/contracts/MerchantPlusSwap.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./Token.sol";
+
+contract MerchantPlusSwap {
+  string public name = "Merchant Plus Exchange";
+  Token public token;
+  uint public rate = 100;
+
+}
