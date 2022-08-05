ffddec05aa336b0a5ff62889cc4113d1dcb9cb4a
amirshapkota <shapkotaamir0@gmail.com>
2022-08-05 21:17:21 +0545

    Token Details Created Token.sol and added Token Details

---
+++ blockchain/contracts/Token.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Token {
+
+    string  public name = "Merchant Plus Token";
+    string  public symbol = "MPT";
+    uint256 public totalSupply = 1000000000000000000000000;
+    uint8   public decimals = 18;
+
+}
