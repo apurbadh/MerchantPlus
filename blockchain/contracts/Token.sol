32db05de57164d6239a3ece202874dae196992a7
amirshapkota <shapkotaamir0@gmail.com>
2022-08-05 21:56:25 +0545

    Events Created events to alert users about the transaction

--- blockchain/contracts/Token.sol
+++ blockchain/contracts/Token.sol
@@ -7,6 +7,18 @@ contract Token {
     uint256 public totalSupply = 1000000000000000000000000;
     uint8   public decimals = 18;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
@@ -21,12 +33,16 @@ contract Token {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
+        emit Transfer(msg.sender, _to, _value);
+
         return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
 
         allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
 
         return true;
     }
@@ -39,6 +55,8 @@ contract Token {
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
         allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
         
         return true;
     }
